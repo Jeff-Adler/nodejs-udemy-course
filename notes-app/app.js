854f974c2f88abd1378d4bdd0bbb23472308f260
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-16 12:02:26 -0500

    added body to add command

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,13 +9,18 @@ yargs
         describe: 'Add a new note',
         builder: {
             title: {
-                describe: "Note title",
+                describe: 'Note title',
                 demandOption: true,
                 type: 'string',
+            },
+            body: {
+                describe: 'Note body',
+                demandOption: true,
+                type: 'string'
             }
         },
         handler: (argv) => {
-            console.log(`Title: ` + argv.title)
+            console.log(`Title: ` + argv.title + `\nBody: ` + argv.body)
         }
     })
     //Remove a note
