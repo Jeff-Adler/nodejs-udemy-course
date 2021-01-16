70915c8f46cbedc9527005fb485cdf5ba262b490
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-16 11:44:14 -0500

    setup basic notes commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,46 @@
 const chalk = require(`chalk`)
-
+const yargs = require(`yargs`)
 const getNotes = require(`./notes.js`)
 
-// console.log(getNotes())
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: () => {
+        console.log(`Adding a new note!`)
+    }
+})
 
-console.log(chalk.green.bold.inverse("Success!"))
+//Remove a note
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: () => {
+        console.log(`Removing the note`)
+    }
+})
+
+//List a note
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: () => {
+        console.log(`Listing out all notes`)
+    }
+})
+
+//Read a note
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: () => {
+        console.log(`Reading a note`)
+    }
+})
+
+
+
+// add, remove, read, list notes
+
+
+console.log(yargs.argv)
