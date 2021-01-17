961f7edb0f9e2f1ee06bb0e359364c068d941e73
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-17 11:02:31 -0500

    added body.response destructure

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,14 +3,14 @@ const request = require(`postman-request`)
 const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherstack.com/current?access_key=339699be72c5f4f7cb7f52f8bbf3168b&query=${latitude},${longitude}&units=f`
 
-    request({url , json : true}, (error, response) => {
+    request({url , json : true}, (error, {body}) => {
         if (error) {
             callback(`Unable to connect to weather services.`, undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback(`Unable to find location.`, undefined)
         } else {
-            const {temperature, feelslike:feelsLike} =  response.body.current
-            const [weatherDescriptions] = response.body.current.weather_descriptions
+            const {temperature, feelslike:feelsLike} =  body.current
+            const [weatherDescriptions] = body.current.weather_descriptions
             callback(undefined, {
                 temperature,
                 feelsLike,
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,14 +3,14 @@ const request = require(`postman-request`)
 const geocode = (address,callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiamFwbGlza2luIiwiYSI6ImNrazE3bWM4dzBvaXYyb3QxcmRlaG10aDAifQ.NKvj5Bde9ScJ_aGRtXGcog&limit=1`
     
-    request({url , json : true}, (error,response) => {
+    request({url , json : true}, (error,{body}) => {
         if (error) {
             callback(`Unable to connect to location services!`, undefined)
-        } else if (response.body.features.length === 0) {
+        } else if (body.features.length === 0) {
             callback(`Unable to find location. Try another search.`, undefined)
         } else {
-            const [longitude, latitude] = response.body.features[0].center
-            const {place_name: location} = response.body.features[0]
+            const [longitude, latitude] = body.features[0].center
+            const {place_name: location} = body.features[0]
             callback(undefined, {
                 latitude,
                 longitude,
