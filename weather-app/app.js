44b16663fe4127f8d0dd62db3c7427cd26abff31
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-17 08:59:31 -0500

    setup customized url request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,8 @@
 
 const request = require(`postman-request`)
 
-const url = `http://api.weatherstack.com/current?access_key=339699be72c5f4f7cb7f52f8bbf3168b&query=37.8267,-122.4233`
+const url = `http://api.weatherstack.com/current?access_key=339699be72c5f4f7cb7f52f8bbf3168b&query=37.8267,-122.4233&units=f`
 
 request({url : url, json: true}, (error, response) => {
-    console.log(response.body.current.weather_descriptions + ' It is currently ' + 
-    response.body.current.temperature + ' degrees out.')
+    console.log(`It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`)
 })
