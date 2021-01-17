b11bcfd1fad9f86bb57f207bf84501328c1399bb
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-17 16:14:37 -0500

    completed weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,11 +1,28 @@
 console.log('Client side javascript file is loaded.')
 
-fetch('http://localhost:3000/weather?address=boston')
-    .then(response => response.json())
-    .then(data => {
-        if (data.error) console.log(data.error)
-        else {
-            console.log(data.location)
-            console.log(data.weatherDescriptions)
-        }
+document.addEventListener('DOMContentLoaded', e => {
+    const weatherForm = document.querySelector('form')
+    const search = document.querySelector('input')
+    const messageOne = document.querySelector('#message-1')
+    const messageTwo = document.querySelector('#message-2')
+    
+    weatherForm.addEventListener('submit', e => {
+        e.preventDefault()
+        
+        messageOne.textContent = "Loading..."
+        messageTwo.textContent = ""
+
+
+        const location = search.value
+        fetch(`http://localhost:3000/weather?address=${location}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.error) messageOne.textContent = data.error
+                else {
+                    messageOne.textContent = data.location
+                    messageTwo.textContent = `${data.weatherDescriptions}. Temperature is: ${data.temperature}. Feels like: ${data.feelsLike}.`
+                }
+            })
+
     })
+})
