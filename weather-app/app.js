0e4d34e51ae7d5de38b72ce4465b82279cf9612a
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-16 21:15:21 -0500

    turned on json:true for http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,7 +3,7 @@ const request = require(`postman-request`)
 
 const url = `http://api.weatherstack.com/current?access_key=339699be72c5f4f7cb7f52f8bbf3168b&query=37.8267,-122.4233`
 
-request({url : url}, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url : url, json: true}, (error, response) => {
+    console.log(response.body.current.weather_descriptions + ' It is currently ' + 
+    response.body.current.temperature + ' degrees out.')
 })
