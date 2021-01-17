67e84f085b1c3602bf06a31e0335761508cccc15
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-16 20:29:47 -0500

    setup http request in app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,9 @@
-console.log(`Starting`)
 
-setTimeout(() => {
-    console.log(`2 second timer`)
-},  2000)
+const request = require(`postman-request`)
 
-setTimeout(() => {
-    console.log(`0 second timer`)
-},0)
+const url = `http://api.weatherstack.com/current?access_key=339699be72c5f4f7cb7f52f8bbf3168b&query=37.8267,-122.4233`
 
-console.log(`Stopping`)
+request({url : url}, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
