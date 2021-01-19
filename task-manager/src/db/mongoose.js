903ea5ae13abfb096007d5b5cd8328e882f53d83
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-18 19:17:07 -0500

    user validations

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require(`mongoose`)
+const validator = require(`validator`)
 
-const db = mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -8,20 +9,73 @@ const db = mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     }, 
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error (`Email is invalid`)
+            }
+        }
+    },
     age: {
-        type: Number
+        type: Number,
+        default:0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     }
 })
 
-mongoose.connection.close()
+const me = new User({
+        name: 'James',
+        age: 74,
+        email: "james.bilbo@gmail.com",
+        password: "7777777"
+    })
 
-// const me = new User({
-//     name: 'Jeff',
-//     age: 30,
+me.save()
+.then((data) => console.log(data))
+.catch((error) => console.log("Error!", error))
+
+// const Task = mongoose.model('Task',{
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     completed: {
+//         type: Boolean,
+//         required: true
+//     },
 // })
 
-// me.save()
-// .then(() => console.log(me))
+// const task1 = new Task({
+//     description: 'Leetcode',
+//     completed: false
+// })
+
+// task1.save()
+// .then(() => console.log(task1))
 // .catch((error) => console.log("Error!", error))
+
+
+// mongoose.connection.close()
