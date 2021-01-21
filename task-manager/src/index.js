16fe7e797880cda3f3e9341bf36fd9e335c7c9e3
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-20 19:44:49 -0500

    took out get users and user/:id and amended delete for me only

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,22 +6,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// Custom express middleware to run before route handler
-// app.use((req, res, next) => {
-    // if (req.method === 'GET') {
-    //     res.send('GET requests are disabled')
-    // } else {
-    //     next()
-    // }
-
-    //Allows route handler to run
-    // next()
-// })
-
-// app.use((req,res,next) => {
-//     res.status(503).send(`${req.method} requests are currently disabled.`)
-// })
-
 // parses body of incoming requests as json
 app.use(express.json())
 // uses routes defined in userRouter and taskRouter
@@ -31,15 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-    const token = jwt.sign({ _id: 'abc123' }, 'thisisasecretkey', { expiresIn : '7 days'} )
-    console.log(token)
-
-    const data = jwt.verify(token, 'thisisasecretkey')
-    console.log(data)
-}
-
-// myFunction()
