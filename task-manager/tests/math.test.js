07582b0cb701216029574a3a2f8053a51d9eab28
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-21 16:53:38 -0500

    added temp tests

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -10,9 +10,13 @@ test('Should calculate total with default', () => {
     expect(total).toBe(12.5)
 })
 
-// Goal: Test temperature conversion functions
-//
-// 1. Export both functions and load them into test suite
-// 2. Create "Should convert 32 F to 0 C"
-// 3. Create "Should convert 0 C to 32 F"
-// 4. Run the Jest to test your work!
+test('Should convert 32 F to 0 C', () =>{
+    const temperatureInCelcius = fahrenheitToCelsius(32)
+    expect(temperatureInCelcius).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () =>{
+    const temperatureInFehrenheit = celsiusToFahrenheit(0)
+    expect(temperatureInFehrenheit).toBe(32)
+})
+
